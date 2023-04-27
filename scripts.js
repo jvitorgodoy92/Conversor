ff6bf678c33b6bac86c984499c937869253d813f
jvitorgodoy92 <jvitorgodoy92@gmail.com>
2023-04-27 14:07:31 -0300

    att.(metodo async/await)

--- scripts.js
+++ scripts.js
@@ -1,16 +1,21 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.3
-const euro = 5.74
-const bitcoin = 149307.36
+
 const iene = 0.041
 const pesoArg = 0.026
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-in').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+
 
     realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
